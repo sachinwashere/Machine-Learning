941c9bcfe667355f06df502a39d6e7a2b4d1fe6a
sachinwashere <137610345+sachinwashere@users.noreply.github.com>
2024-04-17 11:25:10 +0530

    Update Sentiment Analysis.py

--- Sentiment Analysis for Web Articles.py
+++ Sentiment Analysis for Web Articles.py
@@ -16,3 +16,14 @@ print(text)
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity #-1 to 1
 print(sentiment)
+
+#For personalized text in text file
+!pip install nltk
+!pip install textblob
+!pip install newspaper3k
+from textblob import TextBlob
+with open('Positive.txt', 'r') as f:
+  text = f.read()
+  blob = TextBlob(text)
+sentiment = blob.sentiment.polarity #-1 to 1
+print(sentiment)
